1304be9f40fc9cc9c15707e6b3b0f7719a073b51
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-10-31 12:24:33 +0530

    updates logic and ui

---
+++ index.js
@@ -0,0 +1,30 @@
+var txtInput = document.querySelector("#txt-input")
+var redBtn = document.querySelector("#red-btn")
+var greenBtn = document.querySelector("#green-btn")
+var blueBtn = document.querySelector("#blue-btn")
+var outputTxt = document.querySelector("#ouput-txt")
+
+//console.log(outputTxt)
+
+
+function changeColor1() {
+    //console.log("clicked")
+    outputTxt.innerHTML = `<p>${txtInput.value}</p>`
+    outputTxt.style.color = "red"
+}
+
+redBtn.addEventListener('click', changeColor1)
+
+function changeColor2() {
+    //console.log("clicked")
+    outputTxt.style.color = "green"
+}
+
+greenBtn.addEventListener('click', changeColor2)
+
+function changeColor3() {
+    //console.log("clicked")
+    outputTxt.style.color = "blue"
+}
+
+blueBtn.addEventListener('click', changeColor3)
